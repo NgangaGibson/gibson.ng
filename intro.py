606dcf6e0f4f4e60a25e1a70bd4fafb9d4b564a6
NgangaGibson <kinuthiagibson7@gmail.com>
2026-03-14 12:38:16 +0300

    initial commit

---
+++ intro.py
@@ -0,0 +1,6 @@
+def multnumbers():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(f"The multiplication of {num1} and {num2} is {num1 * num2}")
+
+multnumbers()  
